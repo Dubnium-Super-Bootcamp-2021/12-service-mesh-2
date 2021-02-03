212e86e6fb5b6288ba7c8e2ba17b6a61ca997049
azriakmalk <azriakmal2222@gmail.com>
2021-02-04 01:15:17 +0700

    Move file redis from db/kv to tugas

--- materi/db/kv/redis.js
+++ materi/db/kv/redis.js
@@ -17,51 +17,13 @@ client.on('connect', () => {
 
 async function main() {
   try {
-    await setAsync('name1', 'budi');
-    await setAsync('name2', 'azri');
-    // const vall = await getAsync('name2');
-    const arr = [];
-
-    //simpan data
-    for (let i = 1; i < 100; i++) {
-      const val = await getAsync(`name${i}`);
-      if (val === null) {
-        await setAsync(`name${i}`, `berhasil${i}`);
-        await setAsync(`alamat${i}`, `jakarta${i}`);
-        await setAsync(`email${i}`, `@gmail.com${i}`);
-        await setAsync(`notelp${i}`, `021${i}`);
-        await setAsync(`bio${i}`, `doyan ngoding ${i}`);
-        break;
-      }
-    }
-
-    //get data
-    for (let i = 1; i < 100; i++) {
-      const name = await getAsync(`name${i}`);
-      const alamat = await getAsync(`alamat${i}`);
-      const email = await getAsync(`email${i}`);
-      const notelp = await getAsync(`notelp${i}`);
-      const bio = await getAsync(`bio${i}`);
-      if (name !== null) {
-        arr.push({ name, alamat, email, notelp, bio });
-      } else {
-        break;
-      }
-    }
-
-    //delete data
-    for (let i = 1; i < 10; i++) {
-      await delAsync(`name${i}`);
-      await delAsync(`alamat${i}`);
-      await delAsync(`email${i}`);
-      await delAsync(`notelp${i}`);
-      await delAsync(`bio${i}`);
-    }
+    await setAsync('name', 'budiman');
+    const val = await getAsync('name');
     // eslint-disable-next-line prettier/prettier
-    const test = JSON.stringify(arr);
+    const test = JSON.stringify({ name: val });
     console.log(test);
-    await delAsync('name1');
-    // client.end(true);
+    await delAsync('name');
+    client.end(true);
   } catch (err) {
     console.error(err);
   }
